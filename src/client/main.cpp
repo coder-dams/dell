0e80e094001bef73df2c3c523bc74d3ab8b844bc
alexlepo <alexandre.lepoivre@ensea.fr>
2020-11-17 14:51:17 +0100

    case enginev0 main

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -30,6 +30,10 @@ int main(int argc,char* argv[])
         {
 	return TMap();
 	}
+	else if (strcmp(argv[1], "engine") == 0)
+        {
+	return 0;
+	}
 	else
 	{
 	    cout << "Usage : ./client hello or ./client render" << endl;
