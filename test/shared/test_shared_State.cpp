10183b7dfa4d88550354b9d405104af6c5f86118
Abdoulaye Dama <abdoulayedama98@gmail.com>
2020-11-30 09:35:12 +0100

    Character's tests done

--- test/shared/test_shared_State.cpp
+++ test/shared/test_shared_State.cpp
@@ -36,6 +36,28 @@ BOOST_AUTO_TEST_CASE(TestStateClasses)
     BOOST_CHECK_EQUAL(mySpellSet.getDamage(), THROW_DMG);
     }
 
+    //Characters test
+    {
+    Spells mySpellSet;
+    Character c1{PLAYER, "Hamzalemsnap", 0, 0, 1, mySpellSet};
+    Character c2{BOWMAN, "Rachidox", 5, 5, 2, mySpellSet};
+    Character c3{SKELETON, "Lepo", 10, 10, 3, mySpellSet};
+    Character c4{SORCERER, "Abdoulaye", 15, 15, 4, mySpellSet};
 
+    BOOST_CHECK_EQUAL(c1.getType(), PLAYER);
+    BOOST_CHECK_EQUAL(c1.getStats().health, 150);
+
+    c1.getStats().setExperience(100);
+    c1.LevelUp();
+    BOOST_CHECK_EQUAL(c1.getStats().getLevel(), 2);
+    BOOST_CHECK_EQUAL(c1.getSpells().getDamage(), 30);
+    BOOST_CHECK(!c1.isMapCell());
+
+    BOOST_CHECK_EQUAL(c1.getStatus(), WANDERING);
+    c1.setStatus(FIGHTING);
+    BOOST_CHECK_EQUAL(c1.getStatus(), FIGHTING);
+
+
+    }
 
 }
