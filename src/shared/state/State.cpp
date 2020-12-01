ab55da8223b3b48705276bce20c6b482caf37ca1
RachidEddaaif <rachideddaaif@gmail.com>
2020-12-01 01:07:18 +0100

    State.cpp modification [initializeCharacters():void ]

--- src/shared/state/State.cpp
+++ src/shared/state/State.cpp
@@ -35,10 +35,10 @@ std::vector<std::unique_ptr<Character>> &State::getCharacters()
 void State::initializeCharacters (){
 
 	Spells Spell1;
-	std::unique_ptr<Character> Char1(new Character(BOWMAN, "bow", 15, 15,1,Spell1 	));
+	std::unique_ptr<Character> Char1(new Character(BOWMAN, "bow", 15, 15,Spell1 ));
 	characters.push_back(move(Char1));
 	Spells Spell2;
-	std::unique_ptr<Character> Char2(new Character(PLAYER, "pla", 15, 14,1,Spell2 	));
+	std::unique_ptr<Character> Char2(new Character(PLAYER, "pla", 15, 14,Spell2 ));
 	characters.push_back(move(Char2));
    	cout << "Characters created\n";
 }
