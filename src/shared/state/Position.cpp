c1e1dd704f4f6dd3946ceb202e038320e1cbd505
Abdoulaye Dama <abdoulayedama98@gmail.com>
2020-12-01 11:30:21 +0100

    State update

--- src/shared/state/Position.cpp
+++ src/shared/state/Position.cpp
@@ -48,4 +48,18 @@ int Position::distance(Position& position1, Position& position2)
 	return abs(position1.getX() - position2.getX()) + abs(position1.getY() - position2.getY());
 }
 
+std::vector<Position> Position::nearPositions (int x, int y) {
+
+	vector<Position> nP;
+	// J'aurais pu utilisé up, down, left, right mais ils ont déjà utilisés
+	Position upside{this->getX(), this->getY() + 1};
+	Position downside{this->getX(), this->getY() - 1};
+	Position leftside{this->getX() - 1, this->getY()};
+	Position rightside{this->getX() + 1, this->getY()};
+	nP.push_back(move(upside));
+	nP.push_back(move(leftside));
+	nP.push_back(move(downside));
+	nP.push_back(move(rightside));
+
+}
 
