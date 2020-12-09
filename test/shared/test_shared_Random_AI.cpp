442a47c76dd4265b185ee5a65788ee257845d464
alexlepo <alexandre.lepoivre@ensea.fr>
2020-12-09 11:25:24 +0100

    test rand ai

---
+++ test/shared/test_shared_Random_AI.cpp
@@ -0,0 +1,32 @@
+
+#include <boost/test/unit_test.hpp>
+#include <vector>
+#include "../../src/shared/ai.h"
+#include "../../src/shared/state.h"
+#include "../../src/shared/engine.h"
+
+using namespace ::ai;
+using namespace ::engine;
+using namespace ::state;
+using namespace std;
+BOOST_AUTO_TEST_CASE(TestStaticAssert)
+{
+    BOOST_CHECK(1);
+}
+
+BOOST_AUTO_TEST_CASE(TestEngineNamespace)
+{
+    {
+        srand(time(0));
+        engine::Engine ngine{"game"};
+
+        RandomAI rai;
+        
+        rai.setPlayerNumber(1);
+	BOOST_CHECK_EQUAL(1, rai.getPlayerNumber());
+
+        rai.run(ngine);
+    }
+}
+
+/* vim: set sw=2 sts=2 et : */
