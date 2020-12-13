87437c26d45beb7d2ea28827fcdf978a2420aac2
Abdoulaye Dama <abdoulayedama98@gmail.com>
2020-12-13 09:47:02 +0100

    Engine test files set up

---
+++ test/shared/test_shared_AttackCommand.cpp
@@ -0,0 +1,11 @@
+#include <boost/test/unit_test.hpp>
+#include <vector>
+#include "../../src/shared/engine/Engine.h"
+
+using namespace ::engine;
+using namespace std;
+BOOST_AUTO_TEST_CASE(TestStaticAssert)
+{
+    BOOST_CHECK(1);
+}
+
