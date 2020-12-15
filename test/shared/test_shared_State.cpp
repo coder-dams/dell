0e449e60d68062ad32c6616a7a40181dacfbbbf2
alexlepo <alexandre.lepoivre@ensea.fr>
2020-12-15 14:59:20 +0100

    splitting state and spells

--- test/shared/test_shared_State.cpp
+++ test/shared/test_shared_State.cpp
@@ -12,23 +12,7 @@ BOOST_AUTO_TEST_CASE(TestStaticAssert)
 BOOST_AUTO_TEST_CASE(TestStateClasses)
 
 {
-    //Spells test
-    {
-    Spells mySpellSet {};
-
-    mySpellSet.setDamage(100);
-    mySpellSet.setRange(100);
-    mySpellSet.setCost(100);
-    BOOST_CHECK_EQUAL(100, mySpellSet.getDamage());
-    BOOST_CHECK_EQUAL(100, mySpellSet.getRange());
-    BOOST_CHECK_EQUAL(100, mySpellSet.getCost());
-
-    mySpellSet.SetSpell(PUNCH);
-    mySpellSet.SetSpell(IGNITE);
-    mySpellSet.setID(THROW);
-    mySpellSet.SetSpell(mySpellSet.getID());
-
-    }
+    
 
 	// State
 	{
