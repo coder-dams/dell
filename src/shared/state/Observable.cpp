d029479723eda11721de04c9f6af119fcb121e0a
RachidEddaaif <rachideddaaif@gmail.com>
2020-12-29 23:58:21 +0100

    Adding Observers to State

--- src/shared/state/Observable.cpp
+++ src/shared/state/Observable.cpp
@@ -1,13 +1,15 @@
 #include "Observable.h"
 #include <iostream>
-#include "IObserver.h"
 
 using namespace state;
 using namespace std;
 
-//void Observable::registerObserver (IObserver* o)
-//{
-//	observers.push_back(o);
-//}
+void Observable::registerObserver (IObserver* observer){
+	observers.push_back(observer);
+}
 
-
+void Observable::notifyObservers (const StateEvent& sEvent, State& state){
+	for(auto observer : observers){
+		observer->stateChanged(sEvent, state);
+	}
+}
