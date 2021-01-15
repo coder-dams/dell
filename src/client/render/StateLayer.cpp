3f14c8184e77c8b10ad072787f973778cfd8807b
HamzaLEMGADAR <hamza.lemgadar@ensea.fr>
2021-01-15 10:57:51 +0100

    Render modif and /client engine playing 2 rounds

--- src/client/render/StateLayer.cpp
+++ src/client/render/StateLayer.cpp
@@ -12,31 +12,43 @@ using namespace render;
 using namespace std;
 using namespace state;
 
+
 StateLayer::StateLayer (state::State& state, sf::RenderWindow& window, std::string env) : window(window)
 {
-    font.loadFromFile("../res/text_textures/neo-latina-demo-FFP.ttf");
 
-    std::unique_ptr<Tile> tileMapCell(new Tile(TileID::MAPCELL));
     
 }
 
-void StateLayer::initLayer(state::State &state, const int *level)
+void StateLayer::initLayer(state::State &state)
 {
-    //render::LoadLayer *layer;
-    //layer->loadTextures(state,"../res/snow-expansion.png", sf::Vector2u(16, 16),level, 30, 30);
+    LoadLayer layer_1, layer_2,layer_3, layer_ui;
+    
+    layer.clear();
+    layer_1.loadTextures(state,"../res/snow-expansion.png", sf::Vector2u(16, 16),LoadLayer::MakeLayer_1(), 30, 30);
+    layer.push_back(layer_1);
+    layer_2.loadTextures(state,"../res/snow-expansion.png", sf::Vector2u(16, 16),LoadLayer::MakeLayer_2(), 30, 30);
+    layer.push_back(layer_2);
+    layer_ui.loadTextures(state,"../res/ui_textures/ui.png", sf::Vector2u(16, 16),LoadLayer::MakeLayer_UI(), 30, 30);
+    layer.push_back(layer_ui);
+    layer_3.loadTextures(state,"../res/snow-expansion.png", sf::Vector2u(16, 16),state.cMap, 30, 30);
+    layer.push_back(layer_3);
+
+
 }
 
 
-std::vector<std::unique_ptr<LoadLayer>> &StateLayer::getLayer()
+std::vector<LoadLayer> &StateLayer::getLayer()
 {
-    std::vector<std::unique_ptr<LoadLayer>> &refLayer = layer;
+    std::vector<LoadLayer> &refLayer = layer;
     return refLayer;
 }
 
 
 void StateLayer::draw(sf::RenderWindow &window)
 {
-    
-    draw(window);
+    window.clear();
+    for(size_t k=0;k<layer.size();k++){
+        window.draw(layer[k]);
+    }
     window.display();
 }
